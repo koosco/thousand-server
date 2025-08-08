8df7593c509e295fa17d1b8025259d639dc758af
Koo <koosco136@gmail.com>
2025-08-09 02:56:39 +0900

    add health check api

--- src/main/java/koosco/practice/thousandserver/common/api/HealthCheckController.java
+++ src/main/java/koosco/practice/thousandserver/common/api/HealthCheckController.java
@@ -1,15 +1,24 @@
 package koosco.practice.thousandserver.common.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class HealthCheckController {
 
-    @GetMapping
+    @GetMapping("/api/health")
+    public String helloword() {
+        log.info("/api/health called");
+        return "Hello world";
+    }
+
+    @GetMapping("/health")
     public String healthCheck() {
+        log.info("/health called");
         return "OK";
     }
 }
